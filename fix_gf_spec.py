56ca0ba5dafb9a787ce2de23bbcd8b48f1e37dbc
Marc Foley <m.foley.88@gmail.com>
2016-08-01 10:21:09 +0200

    first release: super draft! only use if you are familiar with the spec https://github.com/googlefonts/gf-docs/blob/master/ProjectChecklist.md

---
+++ fix_gf_spec.py
@@ -0,0 +1,14 @@
+'''
+Fix/add requirements from ProjectChecklist.md
+'''
+def main():
+    # Add README file if it does not exist
+
+    font = Glyphs.font
+    font.customParameters['license'] = 'This Font Software is licensed under the SIL Open Font License, Version 1.1. This license is available with a FAQ at: http://scripts.sil.org/OFL'
+    font.customParameters['licenseURL'] = 'http://scripts.sil.org/OFL'
+    font.customParameters['fs']
+
+
+if __name__ == '__main__':
+    main()
