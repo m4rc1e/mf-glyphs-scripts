bdd9f9875f4be7cdf06ddd80c223745412acf55d
Marc Foley <m.foley.88@gmail.com>
2016-08-17 21:35:12 +0200

    improved ui

--- QA/check_family.py
+++ QA/check_family.py
@@ -1,5 +1,13 @@
+#MenuTitle: Check Font
 import vanilla
+import os
+import sys
+import yaml
+import re
 
+sys.path.append('/Users/marc/Library/Application Support/Glyphs/Scripts/mf-glyphs-scripts')
+import find_duplicate_glyphs
+from test_gf_spec import check_family_name
 '''
 
 Check family for GlyphsApp
@@ -39,49 +47,95 @@ __version__ = 0.1
 __author__ = 'Marc Foley'
 
 
-class UI(object):
+class GlyphsUI(object):
 	'''Dialog for enabling/disabling checks'''
-	def __init__(self):
+	def __init__(self, config_file):
 		self.w = vanilla.FloatingWindow((330, 500), "QA Selected Font", minSize=(300,500), maxSize=(1000,700))
 		
-		# Check Meta data
-		self.w.text_1 = vanilla.TextBox((15-1, 12+2, 75, 14), "Meta Data:", sizeStyle='small')
-		self.w.rule1 = vanilla.HorizontalLine((15-1, 26, 300, 14))
-		self.w.fstype = vanilla.CheckBox((15-1, 38, 300, 20), 'Check fsType = 0 (installable)', value=True)
-		self.w.vendor_id = vanilla.CheckBox((15-1, 58, 300, 20), 'Check vendorID exists', value=True)
-		self.w.license = vanilla.CheckBox((15-1, 78, 300, 20), 'Check license is correct', value=True)
-		self.w.license_url = vanilla.CheckBox((15-1, 98, 300, 20), 'Check license url is correct', value=True)
-		self.w.name = vanilla.CheckBox((15-1, 118, 300, 20), 'Check font name has only ASCII chars', value=True)
-		self.w.upm = vanilla.CheckBox((15-1, 138, 300, 20), 'Check font upm is 1000', value=True)
-
-		# Check Vertical Metrics
-		self.w.text_2 = vanilla.TextBox((15-1, 188, 300, 14), "Vertical Metrics:", sizeStyle='small')
-		self.w.rule2 = vanilla.HorizontalLine((15-1, 202, 300, 14))
-		self.w.metrics_fam_vals = vanilla.CheckBox((15-1, 214, 300, 20), "Check instances/masters have same values", value=True)
-		self.w.metrics_125 = vanilla.CheckBox((15-1, 234, 300, 20), 'Check fonts pass 125% rule', value=True)
-		self.w.metrics_khaled = vanilla.CheckBox((15-1, 254, 300, 20), "Check fonts pass Khaled's schema", value=True)
-		self.w.metrics_kalapi = vanilla.CheckBox((15-1, 274, 300, 20), "Check fonts pass Kalapi's schema", value=False)
+		self.leading = 14
+		self.head_count = 0
+		
+		
+		self._heading('Meta Data')
+		# iterate over config file and add each entry
+		for key in config_file:
+			self._checkbox(key, '%s' % key)
+		
+		# Vertical Metrics
+		self._heading('Vertical Metrics:')
+		self._checkbox('metrics_fam_vals', "Instances/Masters have same values")
+		self._checkbox('metrics_125', "Fonts pass legacy 125pc rule")
+		self._checkbox("metrics_khaled", "Fonts pass Khaled's schema")
+		self._checkbox("metrics_kalapi", "Fonts pass Kalapi's schema", value=False)
 
 		# Check Glyphs
-		self.w.text_3 = vanilla.TextBox((15-1, 324, 300, 14), "Glyphs:", sizeStyle='small')
-		self.w.rule3 = vanilla.HorizontalLine((15-1, 338, 300, 14))
-		self.w.glyph_names = vanilla.CheckBox((15-1, 350, 300, 20), "Check glyph names", value=True)
-		self.w.glyph_no_dups = vanilla.CheckBox((15-1, 370, 300, 20), "Check no duplicate glyphs", value=True)
-		self.w.glyph_nbspace_space = vanilla.CheckBox((15-1, 390, 300, 20), "Check nbspace and space are same width", value=True)
-		self.w.button = vanilla.Button((15-1, 450, 300, 20), "Press me", callback=self.buttonCallback)
+		self._heading('Glyphs:')
+		self._checkbox('glyph_names', "Glyph names")
+		self._checkbox('glyph_no_dups', "No duplicate glyphs")
+		self._checkbox('glyph_nbspace_space', "nbspace and space are same width")
+
+		# Check 
+		self.w.button = vanilla.Button((14, self.leading+40, 300, 20), "Check", callback=self.buttonCallback)
 		self.w.open()
+	
+	def _heading(self, title):
+		self.leading += 20
+		setattr(self.w, 'text%s' % self.head_count, vanilla.TextBox((14, self.leading, 300, 14), title, sizeStyle='small'))
+		self.leading += 12
+		self.head_count += 1
+		setattr(self.w, 'rule%s' % self.head_count, vanilla.HorizontalLine((14, self.leading, 300, 14)))
+		self.leading += 12
+	
+	def _checkbox(self, attr,  title, value=True):
+		setattr(self.w, attr, vanilla.CheckBox((14, self.leading, 300, 20), title, value=value))
+		self.leading += 20
 		
 	def buttonCallback(self, sender):
-		print "You pressed the button!"	
-		
+		main(**self.w.__dict__)
 
 
+def check_field(key, yml, font, fix=False):
+	'''Check if a font's attribute matches the yml document'''
+	if 'any' in str(yml):
+		print 'PASS: font %s has attribute' % key
+	elif yml != font:
+		print 'ERROR: font %s is not equal to yml %s' % (key, key)
+	else:
+		print 'PASS: font %s is equal to yml %s' % (key, key)
+	if fix:
+		font = yml
 
-def main():
-	ui = UI()
-	ui.w.open()
-# 	print ui.w.metrics_kalapi.get()
+
+def font_field(font, key):
+	if hasattr(font, key):
+		return getattr(font, key)
+	if key in font.customParameters:
+		return font.customParameters[key]
+	return None
+
+
+def main_glyphs():
+	qa_spec = yaml.safe_load(open('/Users/marc/Library/Application Support/Glyphs/Scripts/mf-glyphs-scripts/QA/qa.yml', 'r'))
+	ui = GlyphsUI(qa_spec)
+
+
+def main(**kwargs):
+	font = Glyphs.font
+
+	qa_spec = yaml.safe_load(open('/Users/marc/Library/Application Support/Glyphs/Scripts/mf-glyphs-scripts/QA/qa.yml', 'r'))
+	if 'glyph_no_dups' in kwargs and kwargs['glyph_no_dups'].get() == 1:
+		find_duplicate_glyphs.find([g.name for g in font.glyphs])
+
+	if 'name' in kwargs and kwargs['name'].get() == 1:
+		check_family_name(font.familyName)
+
+	print '***Check Meta Data***'
+	for key in qa_spec:
+		font_attrib = font_field(font, key)
+		check_field(key, qa_spec[key], font_attrib)
+		else:
+			print ('ERROR YML DOC: Attribute %s does not exist for font' % key)
 
 
 if __name__ == '__main__':
-	main()
+	main_glyphs()
