e1ee663faf5abc09d7c920e04c98d535090ab140
Marc Foley <m.foley.88@gmail.com>
2016-08-28 19:56:54 +0200

    fix spec: remove glyph order and panose

--- Fixes/fix_gf_spec.py
+++ Fixes/fix_gf_spec.py
@@ -5,11 +5,15 @@ def main():
     # Add README file if it does not exist
 
     font = Glyphs.font
+    font.date = datetime.now()
     font.customParameters['license'] = 'This Font Software is licensed under the SIL Open Font License, Version 1.1. This license is available with a FAQ at: http://scripts.sil.org/OFL'
     font.customParameters['licenseURL'] = 'http://scripts.sil.org/OFL'
     font.customParameters['fsType'] = 0
     font.customParameters['Use Typo Metrics'] = True
-    
+    if 'panose' in font.customParameters:
+        del font.customParameters['panose']
+    if 'glyphOrder' in font.customParameters:
+        del font.customParameters['glyphOrder']
 
 
 if __name__ == '__main__':
