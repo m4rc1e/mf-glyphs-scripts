89adc415b67814296738822089490c498169b483
Marc Foley <m.foley.88@gmail.com>
2016-08-28 22:44:30 +0200

    glyphs: Moved uni00a0_width to metrics

--- QA/glyphs.py
+++ QA/glyphs.py
@@ -3,12 +3,13 @@ import unicodedata as uni
 
 IGNORE_GLYPHS_OUTLINE = [
     'uni0000',
+    'NULL',
 ]
 
 
 def find_duplicates(font_glyphs):
     '''Check if there are duplicate glyphs'''
-    print '***Find Duplicate glyphs in selected font***'
+    print '**Find Duplicate glyphs in selected font**'
     glyphs_count = Counter(font_glyphs)
     if len(set(glyphs_count.values())) >= 2:
         for glyph in glyphs_count:
@@ -21,7 +22,7 @@ def find_duplicates(font_glyphs):
 def outlines_missing(font):
     '''Check if glyphs are missing outlines or composites.
     Only works on glyphs which have unicodes'''
-    print '***Check Glyphs have outlines or components***'
+    print '**Check Glyphs have outlines or components**'
     masters = font.masters
 
     for i, master in enumerate(masters):
@@ -38,25 +39,3 @@ def outlines_missing(font):
                 print "ERROR: %s master's %s should have outlines or components\n" % (master.name, glyph)            
         else:
             print "PASS: %s master's glyphs have components or outlines\n" % master.name
-
-
-def uni00a0_width(font, masters, fix=False):
-    '''Set nbspace to same width as space'''
-    print '***Checking space and nbspace have same width***'
-    for id, master in enumerate(masters):
-        if font.glyphs['nbspace'].layers[id].width != font.glyphs['space'].layers[id].width:
-            print "ERROR: %s master's nbspace and space are not same width\n" % (master.name)
-        else:
-            print "PASS: %s master's nbspace and space are same width\n" % (master.name)
-
-        if fix:
-            font.glyphs['nbspace'].layers[id].width = font.glyphs['space'].layers[id].width
-            print 'Now equal widths! space=%s, 00A0=%s' %(
-                font.glyphs['space'].layers[id].width,
-                font.glyphs['nbspace'].layers[id].width
-            )
-
-
-if __name__ == '__main__':
-    font = Glyphs.font
-    find([g.name for g in font.glyphs])
