93fdd23402984c106ec72d671d860215e583c824
Lasse Fister <commander@graphicore.de>
2016-08-31 06:38:03 -0700

    fstype is returned as a string, not number on my machine

--- Google Fonts/test_gf_spec.py
+++ Google Fonts/test_gf_spec.py
@@ -131,7 +131,7 @@ def check_license_url_string(family_license_url):
 
 def check_family_fstype(font_fstype):
     print('***Check fsType***')
-    if font_fstype == SETTINGS['fstype']:
+    if font_fstype in (SETTINGS['fstype'], str(SETTINGS['fstype'])):
         print('PASS: Family fsType matches %s\n' % SETTINGS['fstype'])
     else:
         print('ERROR: Family fsType does not match %s\n' % SETTINGS['fstype'])
