5f7f4224f31bf0203e7e83e51852ddbcd587ed8e
Marc Foley <m.foley.88@gmail.com>
2017-01-18 14:30:32 +0000

    vertmetrics: module added.
    Objects to help writing tests, regarding vertical metrics issues.

---
+++ Google Fonts/vertmetrics.py
@@ -0,0 +1,41 @@
+VERT_KEYS = [
+    'typoAscender',
+    'typoDescender',
+    'typoLineGap',
+    'winAscent',
+    'winDescent',
+    'hheaAscender',
+    'hheaDescender',
+    'hheaLineGap',
+]
+
+VERT_SETS = {
+    'typo': ['typoAscender', 'typoDescender', 'typoLineGap'],
+    'win': ['winAscent', 'winDescent'],
+    'hhea': ['hheaAscender', 'hheaDescender', 'hheaLineGap'],
+}
+
+
+def shortest_tallest_glyphs(font, *args):
+    '''find the tallest and shortest glyphs in all masters from a list.
+    If no list is given, search all glyphs.'''
+    lowest = 0.0
+    highest = 0.0
+
+    masters_count = len(font.masters)
+
+    if args:
+        glyphs = [font.glyphs[i] for i in args]
+    else:
+        glyphs = font.glyphs
+
+    for glyph in glyphs:
+        for i in range(masters_count):
+            glyph_ymin = glyph.layers[i].bounds[0][-1]
+            glyph_ymax = glyph.layers[i].bounds[-1][-1] + glyph.layers[i].bounds[0][-1]
+            if glyph_ymin < lowest:
+                lowest = glyph_ymin
+            if glyph_ymax > highest:
+                highest = glyph_ymax
+
+    return math.ceil(lowest), math.ceil(highest)
